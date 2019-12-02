f51b7972589c9e28ab30b4ec6de4102a99a65182
thiennguyen <thiennguyen96969696@gmail.com>
2019-12-02 10:58:48 +0700

    upload file xu ly

--- Source.cpp
+++ Source.cpp
@@ -23,3 +23,53 @@ void menu() {
 	cout << "Nhan -99 de thoat." << endl;
 }
 
+void xuLy(HoaDon &hd, DSHoaDon &dshd, int chon) {
+	int vitri;
+
+	switch (chon)
+	{
+	case 1:
+		nhapHoaDon(hd);
+		themHoaDon(dshd, hd);
+		break;
+	case 2:
+		xuatDSHoaDon(dshd);
+		break;
+	case 3:
+		vitri = timTheoMaHD(dshd);
+		if (vitri != -1) {
+			cout << dshd.dshd[vitri].MaHD << "\t" << dshd.dshd[vitri].NgayLap.Ngay << "\t" << dshd.dshd[vitri].NgayLap.Thang << "\t" << dshd.dshd[vitri].NgayLap.Nam << "\t" << dshd.dshd[vitri].TongTien << endl;
+		}
+		else
+			cout << "Khong tim thay." << endl;
+		break;
+	case 4:
+		vitri = timTheoNgayLap(dshd);
+		if (vitri != -1) {
+
+			cout << dshd.dshd[vitri].MaHD << "\t" << dshd.dshd[vitri].NgayLap.Ngay << "\t" << dshd.dshd[vitri].NgayLap.Thang << "\t" << dshd.dshd[vitri].NgayLap.Nam << "\t" << dshd.dshd[vitri].TongTien << endl;
+		}
+		else
+			cout << "Khong tim thay." << endl;
+		break;
+	case 5:
+		xoaTheoMaHD(dshd);
+		break;
+	case 6:
+		xoaTheoNgayLap(dshd);
+		break;
+	case 7:
+		sapXepTheoMaHDTangDan(dshd);
+		break;
+	case 8:
+		sapXepTheoTongTienTangDan(dshd);
+		break;
+	case 9:
+		tinhTongtien(hd, dshd);
+		cout << "Tong tien trong danh sach:" << tinhTongtien(hd, dshd) << endl;
+		break;
+	default:
+		cout << "\n So ban nhap khong hop le" << endl;;
+		
+	}
+}
